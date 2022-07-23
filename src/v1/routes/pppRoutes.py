7ac31fc8aa494a6a79144898db4e36e948f5650d
OzkrOssa <oskrlex17@gmail.com>
2022-07-23 16:49:18 -0500

    add routes to router

--- src/v1/routes/pppRoutes.py
+++ src/v1/routes/pppRoutes.py
@@ -4,6 +4,34 @@ from src.controllers import pppController
 pppRouter = APIRouter()
 
 
-@pppRouter.get("/{IP}/ppp/secret")
-def get_secrets(IP: str):
-    return pppController.get_all_secret(IP)
+@pppRouter.get("/{IP}/ppp/secrets")
+def get_all_secrets(IP: str):
+    return pppController.get_all_secret_users(IP)
+
+@pppRouter.get("/{IP}/ppp/secret/{username}")
+def get_one_secret(IP: str, username: str):
+    return pppController.get_secret_user(IP, username)
+
+@pppRouter.get("/{IP}/ppp/actives")
+def get_all_actives(IP: str):
+    return pppController.get_all_active_users(IP)
+
+@pppRouter.get("/{IP}/ppp/active/{username}")
+def get_one_active(IP: str, username: str):
+    return pppController.get_active_user(IP,username)
+
+
+# -------------Add model for update secret-----------------
+@pppRouter.put("/{IP}/ppp/secret/")
+def update_secret(IP: str, **kwargs):
+    return pppController.update_secret_user(IP,**kwargs)
+
+@pppRouter.delete("/{IP}/ppp/secret/{username}")
+def delete_secret(IP: str, username: str):
+    return pppController.delete_secret_user(IP, username)
+
+
+# -------------Add model for create user-----------------
+@pppRouter.post("/{IP}/ppp/secret/")
+def create_secret(IP: str, **kwargs):
+    return pppController.create_secret_user(IP,**kwargs)
