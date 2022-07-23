6a34fd50ad7672563bc520e8c4cde846a85646a8
OzkrOssa <oskrlex17@gmail.com>
2022-07-23 16:48:43 -0500

    adding services to controller

--- src/controllers/pppController.py
+++ src/controllers/pppController.py
@@ -1,23 +1,43 @@
-from src.services import pppService
+from src.services.pppService import PPPService
 
-def get_all_secret(IP: str):
-    data = pppService.ros_get_secrets(IP)
+def get_all_secret_users(IP: str):
+    data = PPPService(IP).get_secrets()
     return {
         "status": "success",
         "data": data,
     }
 
-def get_all_active():
-    return {"message": "Hello World"}
+def get_all_active_users(IP:str):
+    data = PPPService(IP).get_actives()
+    return {
+        "status": "success",
+        "data": data,
+    }
 
-def update_secret():
-    return {"message": "Hello World"}
+def update_secret_user(IP:str, **kwargs):
+    PPPService(IP).update_secret(**kwargs)
+    return {
+        "status": "success",
+        "message": "Secret updated",
+    }
 
-def delete_secret():
-    return {"message": "Hello World"}
+def delete_secret_user(IP:str, username:str):
+    PPPService(IP).delete_secret(username)
+    return {
+        "status": "success",
+        "message": "Secret deleted",
+    }
 
-def get_secret():
-    return {"message": "Hello World"}
+def get_secret_user(IP:str, username:str):
+    data = PPPService(IP).get_secret(username)
+    return {
+        "status": "success",
+        "data": data,
+    }
 
-def get_active():
-    return {"message": "Hello World"}
+def get_active_user(IP:str, username:str):
+    data = PPPService(IP).get_active(username)
+    return {
+        "status": "success",
+        "data": data,
+    }
